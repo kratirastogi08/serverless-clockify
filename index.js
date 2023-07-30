670b91196b92175029b87a23708007105f492e3f
krati <kratirastogi99196@gmail.com>
2023-07-30 19:18:58 +0530

    as

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v3.0! Your function executed successfully in dev 1234',
+        message: 'Go Serverless v3.0! Your function executed successfully in dev',
         input: event,
       },
       null,
