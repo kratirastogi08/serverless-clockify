4dd91b078303fda8b0ca9c20fd109d2ac469b28d
krati <kratirastogi99196@gmail.com>
2023-07-30 21:57:27 +0530

    done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports.healthCheck = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Howdi modi You are so beautiful Yor are so ugly dev',
+        message: 'Howdi modi You are so beautiful Yor are so ugly dev dev-6',
         input: event,
       },
       null,
