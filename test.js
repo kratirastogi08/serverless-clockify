540bb743c6a33fc43481479e7fce12d1b5dc4262
krati <kratirastogi99196@gmail.com>
2023-07-30 21:46:49 +0530

    done

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ module.exports.test = async (event) => {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: 'Tell me how do you do I am awesomne You are so beautiful',
+          message: 'Tell me how do you do I am awesomne You are so beautiful You are so ugly',
           input: event,
         },
         null,
